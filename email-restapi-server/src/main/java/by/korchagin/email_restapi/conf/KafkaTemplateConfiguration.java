0c51d4e6d7e8b91fbfea2805fd9aadab8cba1fd2
Daniil Korchagin <danlia2000789@gmail.com>
2025-03-09 22:40:47 +0300

    Исправление докер компоуза

---
+++ email-restapi-server/src/main/java/by/korchagin/email_restapi/conf/KafkaTemplateConfiguration.java
@@ -0,0 +1,51 @@
+package by.korchagin.email_restapi.conf;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaProducerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaTemplateConfiguration {
+
+    public static final String EMAIL_TOPIC = "email_topic";
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() throws UnknownHostException {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostAddress());
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        var kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setDefaultTopic(EMAIL_TOPIC);
+        return kafkaTemplate;
+    }
+
+    @Bean
+    public DefaultKafkaProducerFactoryCustomizer serializerCustomizer(ObjectMapper objectMapper) {
+        return producerFactory -> producerFactory.setValueSerializer(new JsonSerializer<>(objectMapper));
+    }
+
+}
