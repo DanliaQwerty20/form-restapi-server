ac93bbda44c71f641e603d24c4b66c4e40c14405
Daniil Korchagin <danlia2000789@gmail.com>
2025-03-10 01:15:17 +0300

    Исправление докер компоуза

--- email-restapi-server/src/main/java/by/korchagin/email_restapi/EmailServiceApplication.java
+++ email-restapi-server/src/main/java/by/korchagin/email_restapi/EmailServiceApplication.java
@@ -1,15 +1,29 @@
 package by.korchagin.email_restapi;
 
+import by.korchagin.email_restapi.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
-public class EmailServiceApplication {
+@Slf4j
+public class EmailServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmailService emailService;
 
     public static void main(String[] args) {
         SpringApplication.run(EmailServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        emailService.sendEmail("danlia2000789@gmail.com", "Test Subject", "Test Message");
+        log.info("Успешная отправка");
     }
 
 }
--- email-restapi-server/src/main/java/by/korchagin/email_restapi/conf/KafkaTemplateConfiguration.java
+++ email-restapi-server/src/main/java/by/korchagin/email_restapi/conf/KafkaTemplateConfiguration.java
@@ -28,7 +28,7 @@ public class KafkaTemplateConfiguration {
     public ProducerFactory<String, String> producerFactory() throws UnknownHostException {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostAddress());
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(ProducerConfig.ACKS_CONFIG, "all");
