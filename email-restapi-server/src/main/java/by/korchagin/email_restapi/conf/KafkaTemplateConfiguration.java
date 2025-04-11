c9ee0b3ef9f339a8d1cf07b03ab2593c878a14c1
Daniil Korchagin <danlia2000789@gmail.com>
2025-04-12 01:20:50 +0300

    Исправление докер компоуза

--- email-restapi-server/src/main/java/by/korchagin/email_restapi/conf/KafkaTemplateConfiguration.java
+++ email-restapi-server/src/main/java/by/korchagin/email_restapi/conf/KafkaTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package by.korchagin.email_restapi.conf;
 
+
 import by.korchagin.email_restapi.dto.Message;
 import lombok.SneakyThrows;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -7,9 +8,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.net.InetAddress;
